282a6c3226302e9184adb7dce0bc45c52dddba8f
hozygaming-wesley <cips@greenwingsgames.com>
2025-04-24 22:37:45 +0800

    feat: 新增 UIMakerConfig 類別及其元資料，並更新 package.json 以反映 UIMaker 名稱和描述

--- Assets/Modules/UIMaker/Scripts/Editor/UIMakerConfig.cs
+++ Assets/Modules/UIMaker/Scripts/Editor/UIMakerConfig.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace Modules.UIMaker
 {
-    [CreateAssetMenu(fileName = "UISpawnerConfig", menuName = "UISpawner/UISpawnerConfig", order = 1)]
+    [CreateAssetMenu(fileName = "UIMakerConfig", menuName = "UIMaker/UIMakerConfig", order = 1)]
     public class UIMakerConfig : ScriptableObject
     {
         public List<GameObject> prefabList = new();
